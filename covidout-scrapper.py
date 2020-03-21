0daa8e0cfc6175673f71c15ccdf87acdcdec93a6
Ishan Rayeen <mishan@apple.com>
2020-03-22 00:09:59 +0530

    created the scrapper

---
+++ covidout-scrapper.py
@@ -0,0 +1,29 @@
+import bs4, requests, smtplib
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def getStats():
+    # Download page
+    getPage = requests.get('https://covidout.in/')
+    getPage.raise_for_status() #if error it will stop the program
+
+    # Parse text for foods
+    html = bs4.BeautifulSoup(getPage.text, 'html.parser')
+    confirmed = html.select('#dashboard > div.number-graph-wrapper > div:nth-child(1) > div > div.card-body.status-confirmed > div.cases-container > h2')[0].text
+    hospitalized = html.select('#dashboard > div.number-graph-wrapper > div:nth-child(2) > div > div.card-body.status-hospitalized > div.cases-container > h2')[0].text
+    icu = html.select('#dashboard > div.number-graph-wrapper > div:nth-child(3) > div > div.card-body.status-icu > div.cases-container > h2')[0].text
+    recovered = html.select('#dashboard > div.number-graph-wrapper > div:nth-child(4) > div > div.card-body.status-recovered > div.cases-container > h2')[0].text
+    died = html.select('#dashboard > div.number-graph-wrapper > div:nth-child(5) > div > div.card-body.status-died > div.cases-container > h2')[0].text
+
+    responseText = f'''Confirmed Cases: {confirmed}
+Hospitalized Cases: {hospitalized}
+In ICU: {icu}
+Recovered Cases: {recovered}
+Deaths: {died}'''
+    return responseText
+
+app.run()
