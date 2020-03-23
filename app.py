6d7ee858dee5c390d7752dca8579d899bed9a48f
Ishan Rayeen <mishan@apple.com>
2020-03-23 12:09:38 +0530

    added patients data

--- app.py
+++ app.py
@@ -63,6 +63,57 @@ def sendPhoto(chatID, responseText):
     url = 'https://api.telegram.org/bot'+ bot_token +'/sendPhoto'
     requests.post(url, json= {"chat_id": chatID, "caption": responseText , "parse_mode":"html", "photo":"AgACAgUAAxkBAAPdXndhK8i3FUI7cFv8PfBYnX-bM3AAAuKpMRukLrhX11QX20YEUJD9qCUzAAQBAAMCAANtAAMh3wQAARgE"})
     
+def get_patients_from_city(ct):
+    url = 'https://api.rootnet.in/covid19-in/unofficial/covid19india.org'
+    resp = requests.get(url)
+    json_patients = resp.json()
+
+
+    responseText = ''
+
+    for pt in json_patients['data']['rawPatientData']:
+        patientId = str(pt['patientId'])
+        reportedOn = pt['reportedOn']
+        city = pt['city']
+        state = pt['state']
+        status = pt['status']
+
+        if city.lower() == ct.lower() :
+            responseText +=f'''Patient ID: {patientId}
+Reported On: {reportedOn}
+City: {city}
+State: {state}
+Status: {status}
+
+'''
+
+    return responseText
+
+def get_patients_from_state(st):
+    url = 'https://api.rootnet.in/covid19-in/unofficial/covid19india.org'
+    resp = requests.get(url)
+    json_patients = resp.json()
+
+
+    responseText = ''
+
+    for pt in json_patients['data']['rawPatientData']:
+        patientId = str(pt['patientId'])
+        reportedOn = pt['reportedOn']
+        city = pt['city']
+        state = pt['state']
+        status = pt['status']
+
+        if state.lower() == st.lower() :
+            responseText +=f'''Patient ID: {patientId}
+Reported On: {reportedOn}
+City: {city}
+State: {state}
+Status: {status}
+
+'''
+
+    return responseText
 
 @app.route('/', methods=['POST'])
 def getStats():
@@ -75,19 +126,36 @@ def getStats():
     print(json_data)
     chatID = json_data['message']['chat']['id']
     responseText = ''
-      
-    if json_data['message']['text'] == '/get_full_stats': 
+    
+    command = json_data['message']['text'].lower()
+    if command == '/get_full_stats': 
         responseText = get_stats_overall(json_statewise)
         responseText += "\n\n\n" + get_stats_statewise(json_statewise)
         sendPhoto(chatID, responseText)
-    elif json_data['message']['text'] == '/get_state_wise':
+    elif command == '/get_state_wise':
         responseText = get_stats_statewise(json_statewise)
         sendMessage(chatID, responseText)
-    elif json_data['message']['text'] == '/get_overall':
+    elif command == '/get_overall':
         responseText = get_stats_overall(json_statewise)
         sendMessage(chatID, responseText)
-    elif json_data['message']['text'] == '/get_helpline':
+    elif command == '/get_helpline':
         sendPhoto(chatID, responseText)
+    elif command.startswith('/patients_from_city'):
+        cmd_split = command.strip().split(' ',1)
+        if len(cmd_split) == 2:
+            city = cmd_split[1]
+            responseText = get_patients_from_city(city)
+        else:
+            responseText = 'invalid command!!'
+        sendMessage(chatID, responseText)
+    elif command.startswith('/patients_from_state'):
+        cmd_split = command.strip().split(' ',1)
+        if len(cmd_split) == 2:
+            state = cmd_split[1]
+            responseText = get_patients_from_state(state)
+        else:
+            responseText = 'invalid command!!'
+        sendMessage(chatID, responseText)
     else:
         responseText = get_help_text()
         sendMessage(chatID, responseText)
