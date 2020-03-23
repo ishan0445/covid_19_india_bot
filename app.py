ae54b18c5fe32b2a6f70a0b195fa72b3de35d107
Ishan Rayeen <mishan@apple.com>
2020-03-23 11:10:09 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ bot_token = os.environ['BOT_TOKEN']
 
 
 def get_stats_overall(json_statewise):
+    print('In method get_stats_overall():')
     # Download page
     confirmed = json_statewise['data']['total']['confirmed']
     recovered = json_statewise['data']['total']['recovered']
@@ -23,7 +24,8 @@ Active Cases: <b>{active}</b>'''
     return responseText
 
 def get_stats_statewise(json_statewise):
-    responseText = ''
+    print('In method get_stats_statewise():')
+    responseText = '<b>State Wise Cases in India:</b>\n<b>State / UT | Confirmed | Recovered | Deaths | Active</b>\n'
     for st in json_statewise['data']['statewise']:
         state = st['state']
         confirmed = st['confirmed']
@@ -31,11 +33,13 @@ def get_stats_statewise(json_statewise):
         deaths = st['deaths']
         active = st['active']
 
-        responseText += str(state) + " | " + str(confirmed) + " | " + str(recovered) + " | " + str(deaths) + " | " + str(active) + "\n"
+        if confirmed != 0:
+            responseText += state + " | " + str(confirmed) + " | " + str(recovered) + " | " + str(deaths) + " | " + str(active) + "\n"
 
     return responseText
 
 def get_help_text():
+    print('In method get_help_text():')
     responseText = '''This bot will give latest stats of COVID-19 Cases in India. 
 The data is collected from :
 covid19india.org
@@ -50,17 +54,19 @@ You can control me by sending these commands:
     return responseText
 
 def sendMessage(chatID, responseText):
+    print('In method sendMessage():')
     url = 'https://api.telegram.org/bot'+ bot_token +'/sendMessage'
     requests.post(url, json= {"chat_id": chatID, "text": responseText , "parse_mode":"html"})
 
 def sendPhoto(chatID, responseText):
+    print('In method sendPhoto:')
     url = 'https://api.telegram.org/bot'+ bot_token +'/sendPhoto'
     requests.post(url, json= {"chat_id": chatID, "caption": responseText , "parse_mode":"html", "photo":"AgACAgUAAxkBAAPdXndhK8i3FUI7cFv8PfBYnX-bM3AAAuKpMRukLrhX11QX20YEUJD9qCUzAAQBAAMCAANtAAMh3wQAARgE"})
     
 
 @app.route('/', methods=['POST'])
 def getStats():
-
+    print('In method getStats():')
     url = 'https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise'
     resp = requests.get(url)
     json_statewise = resp.json()
