10671382e8e017df22b907482fdeccfde4afedd9
Ishan Rayeen <mishan@apple.com>
2020-03-26 00:37:57 +0530

    addeed <pre> for statewise data

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ Active Cases: <b>{active}</b>'''
 
 def get_stats_statewise(json_statewise):
     print('In method get_stats_statewise():')
-    responseText = '<b>State Wise Cases in India:</b>\n<b>STATE | CNFM | RCVD | DETH | ACTV</b>\n'
+    responseText = '<b>State Wise Cases in India:</b>\n<b>STATE | CNFM | RCVD | DETH | ACTV</b>\n<pre>'
     respList = []
     for st in json_statewise['data']['statewise']:
         state = st['state']
@@ -39,6 +39,7 @@ def get_stats_statewise(json_statewise):
             respList.append([state, str(confirmed), str(recovered), str(deaths), str(active)] )
     
     responseText += tabulate(respList, tablefmt='presto')
+    responseText += '</pre>'
     return responseText
 
 def get_help_text():
